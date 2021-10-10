611afb7c87db2327c6960cd163751614345ce7c1
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-11 01:08:19 +0530

    Create Scan.js

---
+++ plugins/Scan.js
@@ -0,0 +1,40 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
+const Neotro = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config');
+const Language = require('../language');
+const Lang = Language.getString('tagall');
+
+if (Config.WORKTYPE == 'private') {
+    Neotro.addCommand({ pattern: 'scan ?(.*)', fromMe: true, desc: Lang.SCAN}, (async (message, match) => { 
+
+        if (match[1] == '') return await message.client.sendMessage(message.jid, Lang.NO, MessageType.text);
+
+        var exists = await message.client.isOnWhatsApp(match[1])
+        if (exists) {
+            await message.client.sendMessage(message.jid, '```' + match[1] + '``` \n' + Lang.SUC + '\n' + exists.jid, MessageType.text);
+        }
+        else {
+            await message.client.sendMessage(message.jid,'```' + match[1] + '``` \n' + Lang.UNSUC, MessageType.text);
+        }
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+    Neotro.addCommand({ pattern: 'scan ?(.*)', fromMe: false, desc: Lang.SCAN}, (async (message, match) => { 
+
+        if (match[1] == '') return await message.client.sendMessage(message.jid, Lang.NO, MessageType.text);
+
+        var exists = await message.client.isOnWhatsApp(match[1])
+        if (exists) {
+            await message.client.sendMessage(message.jid, '```' + match[1] + '``` \n' + Lang.SUC + '\n' + exists.jid, MessageType.text);
+        }
+        else {
+            await message.client.sendMessage(message.jid,'```' + match[1] + '``` \n' + Lang.UNSUC, MessageType.text);
+        }
+    }));
+}
