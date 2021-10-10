daf9f58c8f5f3a98eb9b5dde8b12737d5b3db06e
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-11 04:43:25 +0530

    Update Device.js

--- plugins/Device.js
+++ plugins/Device.js
@@ -37,7 +37,7 @@ msg += '```'
  return await message.client.sendMessage(message.jid, msg, MessageType.text, { quoted: message.data });
 });
 
-    Neotro.addCommand({pattern: 'device ?(.*)', fromMe: true, dontAdCommandList: true, dontAddCommandList: false}, async (message, match) => {
+    Neotro.addCommand({pattern: 'device ?(.*)', fromMe: true, dontAddCommandList: true, dontAddCommandList: false}, async (message, match) => {
     
         const {data} = await axios(`https://zenzapi.xyz/api/gsmarena?query=${match[1]}&apikey=a9a05974d30e`)
         const { status, result } = data
