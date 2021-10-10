7d13f03875fc99697c5fca1c95215e9b3bba8b4b
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-10 16:58:48 +0530

    ©️TEENU-X

---
+++ qr.js
@@ -0,0 +1,57 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
+const chalk = require('chalk');
+const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
+const {StringSession} = require('./Amazone/');
+const fs = require('fs');
+
+async function Amazone () {
+    const conn = new WAConnection();
+    const Session = new StringSession();  
+    conn.version = [2, 2119, 6]
+    conn.logger.level = 'warn';
+    conn.regenerateQRIntervalMs = 50000;
+    
+    conn.on('connecting', async () => {
+        console.log(`${chalk.green.bold('Amazone')}${chalk.blue.bold('Alexa')}
+${chalk.white.italic('Amazone QR session')}
+
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
+    });
+    
+
+    conn.on('open', async () => {
+        var st = Session.createStringSession(conn.base64EncodedAuthInfo());
+        console.log(
+            chalk.green.bold('AMAZONE SESSION: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+        );
+        
+        if (!fs.existsSync('config.env')) {
+            fs.writeFileSync('config.env', `NEUTRO_SESSION="${st}"`);
+        }
+        if (conn.user.jid.startsWith('94')) {
+            await conn.sendMessage(conn.user.jid,st, MessageType.text)
+            await conn.sendMessage(conn.user.jid,'*©️සාර්තකයි. මෙම කේතය අන් අය සමග බෙදානොගන්න.*', MessageType.text)
+            console.log(
+                chalk.blue.bold('installing Node js')
+            );
+        }
+        else {
+            await conn.sendMessage(conn.user.jid,st, MessageType.text)
+            await conn.sendMessage(conn.user.jid,'*Do Not Share This Code With Anyone!*', MessageType.text)
+            console.log(
+                chalk.blue.bold('If you are installing locale, you can start the bot with node bot.js')
+            );
+        }
+        
+        process.exit(0);
+    });
+
+    await conn.connect();
+}
+
+Amazone()
