523deb86b79cc35b5d208665907bec939372e229
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-10 18:02:01 +0530

    Create Base.js

---
+++ Amazone/Base.js
@@ -0,0 +1,21 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
+'use strict';
+
+class Base {
+    constructor(client) {
+        Object.defineProperty(this, 'client', { value: client });
+    }
+
+    _clone() {
+        return Object.assign(Object.create(this), this);
+    }
+    
+    _patch(data) { return data; }
+}
+
+module.exports = Base;
