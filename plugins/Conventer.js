37dd623ef11f9615faba1269791d77cf768c728a
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-10 22:43:55 +0530

    Create Conventer.js

---
+++ plugins/Conventer.js
@@ -0,0 +1,149 @@
+/*Codded by @phaticusthiccy
+Telegram: https://t.me/phaticusthiccy
+Instagram: https://instagram.com/kyrie.baran
+*/
+
+const Neotro = require('../events');
+const {MessageType,Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+const {execFile} = require('child_process');
+const cwebp = require('cwebp-bin');
+const Config = require('../config');
+const cheerio = require('cheerio')
+const FormData = require('form-data')
+const Axios = require('axios');
+const Language = require('../language');
+const Lang = Language.getString('conventer');
+function webp2mp4File(path) {
+    return new Promise(async (resolve, reject) => {
+        const bodyForm = new FormData()
+        bodyForm.append('new-image-url', '')
+        bodyForm.append('new-image', fs.createReadStream(path))
+        await Axios({
+            method: 'post',
+            url: 'https://s6.ezgif.com/webp-to-mp4',
+            data: bodyForm,
+            headers: {
+                'Content-Type': `multipart/form-data boundary=${bodyForm._boundary}`
+            }
+        }).then(async ({ data }) => {
+            const bodyFormThen = new FormData()
+            const $ = cheerio.load(data)
+            const file = $('input[name="file"]').attr('value')
+            const token = $('input[name="token"]').attr('value')
+            const convert = $('input[name="file"]').attr('value')
+            const gotdata = {
+                file: file,
+                token: token,
+                convert: convert
+            }
+            bodyFormThen.append('file', gotdata.file)
+            bodyFormThen.append('token', gotdata.token)
+            bodyFormThen.append('convert', gotdata.convert)
+            await Axios({
+                method: 'post',
+                url: 'https://ezgif.com/webp-to-mp4/' + gotdata.file,
+                data: bodyFormThen,
+                headers: {
+                    'Content-Type': `multipart/form-data boundary=${bodyFormThen._boundary}`
+                }
+            }).then(({ data }) => {
+                const $ = cheerio.load(data)
+                const result = 'https:' + $('div#output > p.outfile > video > source').attr('src')
+                resolve({
+                    status: true,
+                    message: "*👩‍🦰QUEEN-AMAZONE🚀*",
+                    result: result
+                })
+            }).catch(reject)
+        }).catch(reject)
+    })
+}
+
+
+    Neotro.addCommand({pattern: 'mp3$', fromMe: true, desc: Lang.MP4TOAUDİO_DESC}, (async (message, match) => {    
+        const mid = message.jid
+        if (message.reply_message === false) return await message.client.sendMessage(mid, Lang.MP4TOAUDİO_NEEDREPLY, MessageType.text);
+        var downloading = await message.client.sendMessage(mid,Lang.MP4TOAUDİO,MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+
+        ffmpeg(location)
+            .save('output.mp3')
+            .on('end', async () => {
+                await message.client.sendMessage(mid, fs.readFileSync('output.mp3'), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: false});
+            });
+        return await message.client.deleteMessage(mid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    }));
+
+    Neotro.addCommand({pattern: 'photo$', fromMe: true, desc: Lang.STİCKER_DESC}, (async (message, match) => {   
+        const mid = message.jid
+        if (message.reply_message === false) return await message.client.sendMessage(mid, Lang.STİCKER_NEEDREPLY, MessageType.text);
+        var downloading = await message.client.sendMessage(mid,Lang.STİCKER,MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+
+        ffmpeg(location)
+            .fromFormat('webp_pipe')
+            .save('output.jpg')
+            .on('end', async () => {
+                await message.client.sendMessage(mid, fs.readFileSync('output.jpg'), MessageType.image, {mimetype: Mimetype.jpg});
+            });
+        return await message.client.deleteMessage(mid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    }));
+
+
+
+    Neotro.addCommand({pattern: 'mp3$', fromMe: true, dontAdCommandList: true}, (async (message, match) => {    
+        const mid = message.jid
+        if (message.reply_message === false) return await message.client.sendMessage(mid, Lang.MP4TOAUDİO_NEEDREPLY, MessageType.text);
+        var downloading = await message.client.sendMessage(mid,Lang.MP4TOAUDİO,MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+
+        ffmpeg(location)    
+            .save('output.mp3')
+            .on('end', async () => {
+                await message.client.sendMessage(mid, fs.readFileSync('output.mp3'), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: false});
+            });
+        return await message.client.deleteMessage(mid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    }));
+
+    Neotro.addCommand({pattern: 'photo$', fromMe: true, dontAdCommandList: true}, (async (message, match) => {    
+        const mid = message.jid
+        if (message.reply_message === false) return await message.client.sendMessage(mid, Lang.STİCKER_NEEDREPLY, MessageType.text);
+        var downloading = await message.client.sendMessage(mid,Lang.STİCKER,MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+
+        ffmpeg(location)
+            .fromFormat('webp_pipe')
+            .save('output.jpg')
+            .on('end', async () => {
+                await message.client.sendMessage(mid, fs.readFileSync('output.jpg'), MessageType.image, {mimetype: Mimetype.jpg});
+            });
+        return await message.client.deleteMessage(mid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    }));
+}
+    
