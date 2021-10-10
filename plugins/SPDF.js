4803ec56121d02b31b16630c8342f3d8f24a9214
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-11 04:48:25 +0530

    ©️TEENU-X

--- plugins/SPDF.js
+++ plugins/SPDF.js
@@ -27,7 +27,7 @@ let td = Config.WORKTYPE == 'public' ? false : true
 
     }));    
 
-Neotro.addCommand({pattern: 'spdf ?(.*)', fromMe: true, dontAdCommandList: true}, (async (message, match) => {
+Neotro.addCommand({pattern: 'spdf ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 
      if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.SPDF_LINK, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: false }, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "⦁⦁ Amazone: No URL ⦁⦁", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./Anazone/Amazone.png')}}}});
 
