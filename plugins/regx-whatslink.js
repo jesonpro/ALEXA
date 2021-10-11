b373556ea64ae0fffef8015ed51790bbda556ea4
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-11 19:21:02 +0530

    ©️TEENU-X

---
+++ plugins/regx-whatslink.js
@@ -0,0 +1,82 @@
+const Neotro = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const exec = require('child_process').exec;
+const os = require("os");
+const fs = require('fs');
+const Config = require('../config')
+const Language = require('../language');
+const Lang = Language.getString('evaluators');
+const SLang = Language.getString('conventer');
+const NLang = Language.getString('scrapers');
+const googleTTS = require('google-translate-tts');
+const Heroku = require('heroku-client');
+const heroku = new Heroku({
+    token: Config.HEROKU.API_KEY
+});
+
+
+let baseURI = '/apps/' + Config.HEROKU.APP_NAME;
+
+
+async function checkUsAdmin(message, user = message.data.participant) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {     
+        if (member.jid.split("@")[0] == user.split("@")[0] && member.isAdmin) return true; else; return false;
+    });
+    return sonuc.includes(true);
+}
+async function checkImAdmin(message, user = message.client.user.jid) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {     
+        if (member.jid.split("@")[0] == user.split("@")[0] && member.isAdmin) return true; else; return false;
+    });
+    return sonuc.includes(true);
+
+}
+var alllink_var = ''
+async function megalink() {
+    await heroku.get(baseURI + '/config-vars').then(async (vars) => {
+        alllink_var = vars.WHATS_LINK_BLOCK
+    });
+}
+megalink()
+var ldc = ''
+if (Config.LANG == 'AZ') ldc = '*Bağlantı Aşkarlandı!*'
+if (Config.LANG == 'TR') ldc = '*‎Link Tespit Edildi!*'
+if (Config.LANG == 'EN') ldc = '*Link Detected!  link Not allowed here\n Action:Kick \n\n 🕊🕊*'
+if (Config.LANG == 'SI') ldc = '*<===👩‍🦰Amazone Alexa===>*\n*🦹‍♀️_Whatsapp Group Link Block System_\n\n*⛔හේතුව: සමූහයේ Whatsapp Group Link තහනම් කර ඇත.*\n*⚡ක්‍රියාමාර්ගය:ඉවත් කිරීම.*'
+if (Config.LANG == 'ID') ldc = '*Tautan Terdeteksi!*'
+if (Config.LANG == 'PT') ldc = '*Link Detectado!*'
+if (Config.LANG == 'RU') ldc = '*Ссылка обнаружена!*'
+if (Config.LANG == 'HI') ldc = '*लिंक का पता चला!*'
+if (Config.LANG == 'ES') ldc = '*Enlace Detectado!*'
+Neotro.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
+    if (alllink_var == 'true') {
+        let regex1 = new RegExp('bitly.com')
+        let regex2 = new RegExp('https://chat.whatsapp.com')
+        if (regex1.test(message.message)) {
+            var us = await checkUsAdmin(message)
+            var im = await checkImAdmin(message)
+            if (!im) return;
+            if (us) return;
+            await message.client.groupRemove(message.jid, [message.data.participant]);         
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
+        } 
+        else if (regex2.test(message.message)) {
+            var us = await checkUsAdmin(message)
+            var im = await checkImAdmin(message)
+            if (!im) return;
+            if (us) return;
+            await message.client.groupRemove(message.jid, [message.data.participant]);         
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
+        }
+        else if (message.message.match(/((?:[.]tyyfdr)\b)/i)) {
+            var us = await checkUsAdmin(message)
+            var im = await checkImAdmin(message)
+            if (!im) return;
+            if (us) return;
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
+            await message.client.groupRemove(message.jid, [message.data.participant]);
+        }
+    }
+}));
