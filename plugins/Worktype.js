cc431cc29a91cc2edcec452901151e771fb88e70
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-11 19:23:24 +0530

    ©️TEENU-X

---
+++ plugins/Worktype.js
@@ -0,0 +1,59 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
+const Neotro = require('../events');
+const config = require('../config');
+const Heroku = require('heroku-client');
+const Language = require('../language');
+const Lang = Language.getString('amazone');
+
+const heroku = new Heroku({
+    token: config.HEROKU.API_KEY
+});
+let baseURI = '/apps/' + config.HEROKU.APP_NAME;
+
+
+ var W_PUB = ''
+ var W_PRI = ''
+ var W_ADM = ''
+  if (config.LANG == 'EN') {
+
+    W_ADM = '👩‍🦰ᴡᴏʀᴋ ᴛʏᴘᴇ ɪꜱ ᴀᴅᴍɪɴ ɴᴏᴡ' 
+    W_PUB = '👩‍🦰ᴡᴏʀᴋ_ᴛʏᴘᴇ ɪꜱ ɴᴏᴡ ᴘᴜʙʟɪᴄ'
+    W_PRI = '👩‍🦰ᴡᴏʀᴋ_ᴛʏᴘᴇ ɪꜱ ɴᴏᴡ ᴘʀɪᴠᴀᴛᴇ'
+    }
+
+    if (config.LANG == 'SI') {
+
+      W_ADM = '👩‍🦰ඔබේ Bot ඇඩ්මින් ලෙස ක්‍රියාකරයි.'
+      W_PUB = '👩‍🦰ඔබේ Bot පොදු ආකාරයට ක්‍රියාකරයි.'
+      W_PRI = '👩‍🦰ඔබේ Bot පෞද්ගලික ආකාරයට ක්‍රියාකරයි.'
+    }
+
+ Neotro.addCommand({pattern: 'work ?(.*)', fromMe: true, dontAddCommandList: false, desc: Lang.WORK}, (async (message, match) => {
+        if (match[1] == 'public') {
+                await heroku.patch(baseURI + '/config-vars', { 
+                    body: { 
+                        ['WORK_TYPE']: 'public'
+                    } 
+                });
+                await message.sendMessage(W_PUB)
+        } else if (match[1] == 'private') {
+                await heroku.patch(baseURI + '/config-vars', { 
+                    body: { 
+                        ['WORK_TYPE']: 'private'
+                    } 
+                });
+                await message.sendMessage(W_PRI)
+         } else if (match[1] == 'private') {
+                await heroku.patch(baseURI + '/config-vars', { 
+                    body: { 
+                        ['WORK_TYPE']: 'admin'
+                    } 
+                });
+                await message.sendMessage(W_ADM)
+        }
+    }));
