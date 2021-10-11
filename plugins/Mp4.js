594ab0a77aa61e51b3d64e4fcd723fa7bff38c03
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-11 19:07:53 +0530

    ©️TEENU-X

---
+++ plugins/Mp4.js
@@ -0,0 +1,68 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
+const amazone = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const { errorMessage, infoMessage } = require('../helpers');
+const Config = require('../config');
+const Language = require('../language');
+const Lang = Language.getString('amazone');
+const YTV_DESC = "Youtube Video Downloader V2 ."
+const YT_NEED = "*need word!.*"
+const DWLOAD_VID = "*🎭Downloading Your Video...*"
+const YTV_UP = "*🚀Uploading Your Video...*"
+const NO_RESULT = "*🌀can't Find Anything...*"
+
+    amazone.addCommand({ pattern: 'video ?(.*)', fromMe: true, desc: Lang.MP4,  deleteCommand: false}, async (message, match) => {
+
+        const link = match[1]
+    
+        if (!link) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
+        await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text);
+        await axios
+          .get(`https://api.zeks.xyz/api/ytplaymp4/2?apikey=VI6j4t4wCbwoc6Deh5wgrJL2Kt1&q=${link}`)
+          .then(async (response) => {
+            const {
+              link,
+            } = response.data.result
+    
+            const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
+    
+            await message.client.sendMessage(message.jid,YTV_UP,MessageType.text);
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.document, {mimetype: Mimetype.mp4, ptt: false})
+        })
+        .catch(
+          async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
+        )
+      },
+    )
+
+if (Config.WORKTYPE == 'public') {
+    amazone.addCommand({ pattern: 'video ?(.*)', fromMe: false, desc: Lang.MP4}, async (message, match) => {
+
+        const link = match[1]
+    
+        if (!link) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text, {quoted: message.data})
+         await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text)
+        await axios
+          .get(`https://api.zeks.xyz/api/ytplaymp4/2?apikey=VI6j4t4wCbwoc6Deh5wgrJL2Kt1&q=${link}`)
+          .then(async (response) => {
+            const {
+              link,
+            } = response.data.result
+    
+            const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
+    
+            await message.client.sendMessage(message.jid,YTV_UP,MessageType.text, {quoted: message.data});
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.document, {mimetype: Mimetype.mp4, ptt: false})
+        })
+        .catch(
+          async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
+        )
+      },
+    )
+}
