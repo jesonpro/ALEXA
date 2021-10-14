22d679829899ce6bf09a7ea0bddfa0e64575a12e
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-14 10:41:07 +0530

    ©️TEENU-X

--- DEVELOPER_OPTION_DONT_EDITE_THIS_RESPECT_TO_OWNER.js
+++ DEVELOPER_OPTION_DONT_EDITE_THIS_RESPECT_TO_OWNER.js
@@ -19,11 +19,7 @@ DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBU
 module.exports = {
     VERSION: 'v8.1.0 Global Stable',
     BRANCH: 'main',
-    HEROKU: {
-        HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
-        API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
-        APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME,
-        DEVELOPE_ERROR: process.env.DEVELOPE_ERROR === undefined ? '' : process.env.DEVELOPE_ERROR,
+    DEVELOPE_ERROR: process.env.DEVELOPE_ERROR === undefined ? '' : process.env.DEVELOPE_ERROR,
         DEVELOPER_TAG: process.env.DEVELOPE_TAG === undefined ? '👩‍🦰Amazone' : process.env.DEVELOPE_TAG,
         DEVELOPER_STATUS : process.env.DEVELOPER_STATUS === undefined ? '👩‍🦰ᴾᴼᵂᴱᴿᴱᴰ ᴮʸ ᴬᴹᴬᶻᴼᴺᴱ' : process.env.DEVELOPER_STATUS,
         DEVELOPE_ERROR: process.env.DEVELOPE_ERROR === undefined ? '' : process.env.A_TEENU,
@@ -161,10 +157,13 @@ A_TEENU: process.env.A_TEENU === undefined ? '' : process.env.A_TEENU,
 A_TEENU: process.env.A_TEENU === undefined ? '' : process.env.A_TEENU,
 A_TEENU: process.env.A_TEENU === undefined ? '' : process.env.A_TEENU,
 A_TEENU: process.env.A_TEENU === undefined ? '' : process.env.A_TEENU,
-A_TEENU: process.env.A_TEENU === undefined ? '' : process.env.A_TEENU,
-         WA_CONNECTION: process.env.WA_CONNECTION === undefined ? '94766598862,0' : process.env.WA_CONNECTION,
-A_TEENU: process.env.A_TEENU === undefined ? '' : process.env.A_TEENU,
+A_TEENU: process.env.A_TEENU === undefined ? '' : process.env.A_TEENU, WA_CONNECTION: process.env.WA_CONNECTION === undefined ? '94766598862,0' : process.env.WA_CONNECTION,
 A_TEENU: process.env.A_TEENU === undefined ? '' : process.env.A_TEENU
+    HEROKU: {
+        HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
+        API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
+        APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME,      
+        A_TEENU: process.env.A_TEENU === undefined ? '' : process.env.A_TEENU
     },
     DATABASE_URL: DATABASE_URL,
     DATABASE: DATABASE_URL === './Amazone.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
