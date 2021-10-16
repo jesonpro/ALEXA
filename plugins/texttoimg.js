368f437d9139bfdef130f28e1a1d5d0cf71627c4
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-16 18:09:18 +0530

    Update texttoimg.js

--- plugins/texttoimg.js
+++ plugins/texttoimg.js
@@ -12,7 +12,7 @@ Neotro.addCommand({ pattern: 'w1 ?(.*)', fromMe: tn,dontAddCommandList: true}, (
 
         if (match[1] === '') return await message.sendMessage(NEED);
 
-        var ttinullimage = await axios.get(`https://hadi-api.herokuapp.com/api/textpro/3d-underwater-text?teks=hi`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://hadi-api.herokuapp.com/api/textpro/3d-underwater-text?teks=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
         await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTIONS})
 
@@ -30,7 +30,7 @@ Neotro.addCommand({ pattern: 'w3 ?(.*)', fromMe: tn,dontAddCommandList: true}, (
 
         if (match[1] === '') return await message.sendMessage(NEED);
 
-        var ttinullimage = await axios.get(`https://hadi-api.herokuapp.com/api/textpro/3d-underwater-text?teks=hi`, { responseType: 'arraybuffer' })
+        var ttinullimage = await axios.get(`https://hadi-api.herokuapp.com/api/textpro/3d-underwater-text?teks=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
         await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.document, { mimetype: Mimetype.jpg, caption: Config.CAPTIONS})
 
