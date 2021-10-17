0eadeadeb10c44bc5d59cb72d4aa0e69cc0d4dd2
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-17 18:33:50 +0530

    Update texttoimg.js

--- plugins/texttoimg.js
+++ plugins/texttoimg.js
@@ -19,6 +19,11 @@ const NEED = "*🍭YOU MUST TYPE SOME WORDS*"
 const desc = "Text to image Pack";
 let tn = Config.WORKTYPE == 'public' ? false : true
 
+Neotro.addCommand({ pattern: '3dwater ?(.*)', fromMe: tn,dontAddCommandList: true}, (async (message, match) => {
+        if (match[1] === '') return await message.sendMessage(NEED);
+        var ttinullimage = await axios.get(`${config.HTTPS}${config.HLOCK}${config.SUP_HEROKU}${config.ENCRYPTION}${config.TOXIC_T}3d-underwater-text?teks=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTIONS})
+    }));
 
 Neotro.addCommand({ pattern: 'lcube ?(.*)', fromMe: tn,dontAddCommandList: true}, (async (message, match) => {
         if (match[1] === '') return await message.sendMessage(NEED);
