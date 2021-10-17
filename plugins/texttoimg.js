7eee83e16004dad7a5f22e716911f90e03ba4405
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-17 13:11:51 +0530

    ©️TEENU-X

--- plugins/texttoimg.js
+++ plugins/texttoimg.js
@@ -9,11 +9,29 @@ const NEED = "*🍭YOU MUST TYPE SOME WORDS*"
 const desc = "Text to image Pack";
 let tn = Config.WORKTYPE == 'public' ? false : true
 
-Neotro.addCommand({ pattern: 'w1 ?(.*)', fromMe: tn,dontAddCommandList: true}, (async (message, match) => {
+Neotro.addCommand({ pattern: '3dwater ?(.*)', fromMe: tn,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(NEED);
 
         var ttinullimage = await axios.get(`${config.HTTPS}${config.HLOCK}${config.SUP_HEROKU}${config.ENCRYPTION}${config.TOXIC_T}3d-underwater-text?teks=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTIONS})
+
+    }));
+Neotro.addCommand({ pattern: ' ?(.*)', fromMe: tn,dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(NEED);
+
+        var ttinullimage = await axios.get(`${config.HTTPS}${config.HLOCK}${config.SUP_HEROKU}${config.ENCRYPTION}${config.TOXIC_T}?teks=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTIONS})
+
+    }));
+Neotro.addCommand({ pattern: ' ?(.*)', fromMe: tn,dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(NEED);
+
+        var ttinullimage = await axios.get(`${config.HTTPS}${config.HLOCK}${config.SUP_HEROKU}${config.ENCRYPTION}${config.TOXIC_T}?teks=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
         await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTIONS})
 
