4c4530ac17c21b60d9c6574f487ac3c6206db4de
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-19 00:45:13 +0530

    🧜‍♀

--- plugins/VIDEO.js
+++ plugins/VIDEO.js
@@ -1,3 +1,6 @@
+//Api එක උස්සන එකාගෙ මුලු පව්ලම හෙන හතක් වැදිලා 
+//මකබෑවිලාම යන්න ඕනෙ..!!!! eeeew!
+
 const amazone = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
@@ -12,19 +15,19 @@ const DWLOAD_VID = "*🎭Downloading Your Video...*"
 const YTV_UP = "*🚀Uploading Your Video...*"
 const NO_RESULT = "*🌀can't Find Anything...*"
 
-    amazone.addCommand({ pattern: 'tiktok ?(.*)', fromMe: true, deleteCommand: false, desc: Lang.MP4,  deleteCommand: false}, async (message, match) => {
-        const link = match[1]
-        if (!link) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
+    amazone.addCommand({ pattern: 'video ?(.*)', fromMe: true, deleteCommand: false, desc: Lang.MP4,  deleteCommand: false}, async (message, match) => {
+        const linkk = match[1]
+        if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
         await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text);
         await axios
-          .get(`https://bx-${tenu.ON}.herokuapp.com/api/download/tiktok?url=${link}&apikey=${tenu.CCN}`)
+          .get(`https://rei-api.herokuapp.com/api/dl/ytavv2?url=${linkk}`)
           .then(async (response) => {
             const {
-              vid_wm,
-            } = response.data.video_URL
-            const videoBuffer = await axios.get(vid_wm, {responseType: 'arraybuffer'})
+              link,
+            } = response.data.result
+            const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
             await message.client.sendMessage(message.jid,YTV_UP,MessageType.text);
-            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.document, {mimetype: Mimetype.mp4, ptt: false})
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {mimetype: Mimetype.mp4, ptt: false})
         })
         .catch(
           async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
