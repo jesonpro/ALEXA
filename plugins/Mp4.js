c16da0ea403541701441ceb9c563b22ed8b1f292
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-10-30 00:35:51 +0530

    👸Video Fixed

--- plugins/Mp4.js
+++ plugins/Mp4.js
@@ -1,13 +1,12 @@
-/* Copyright (C) 2021 TENUX-Neotro.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
+//Api එක උස්සන එකාගෙ මුලු පව්ලම හෙන හතක් වැදිලා 
+//මකබෑවිලාම යන්න ඕනෙ..!!!! eeeew!
 
 const amazone = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 const { errorMessage, infoMessage } = require('../helpers');
 const Config = require('../config');
+const tenu = require('../hatzu');
 const Language = require('../language');
 const Lang = Language.getString('amazone');
 const YTV_DESC = "Youtube Video Downloader V2 ."
@@ -15,54 +14,26 @@ const YT_NEED = "*need word!.*"
 const DWLOAD_VID = "*🎭Downloading Your Video...*"
 const YTV_UP = "*🚀Uploading Your Video...*"
 const NO_RESULT = "*🌀can't Find Anything...*"
+const tk = Config.WORKTYPE == 'public' ? false : true
 
-    amazone.addCommand({ pattern: 'video ?(.*)', fromMe: true, deleteCommand: false, desc: Lang.MP4,  deleteCommand: false}, async (message, match) => {
-
-        const link = match[1]
-    
-        if (!link) return await 8.client.sendMessage(message.jid,YT_NEED,MessageType.text)
+    amazone.addCommand({ pattern: 'video ?(.*)', fromMe: tk, deleteCommand: false, desc: Lang.MP4,  deleteCommand: false}, async (message, match) => {
+        const linkk = match[1]
+        if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
         await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text);
         await axios
-          .get(`https://api.zeks.xyz/api/ytplaymp4/2?apikey=VI6j4t4wCbwoc6Deh5wgrJL2Kt1&q=${link}`)
+          .get(`https://hardianto-chan.herokuapp.com/api/download/ytdownload?url=${linkk}&apikey=hardianto`)
           .then(async (response) => {
             const {
               link,
             } = response.data.result
-    
             const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
-    
             await message.client.sendMessage(message.jid,YTV_UP,MessageType.text);
-            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.document, {mimetype: Mimetype.mp4, ptt: false})
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {mimetype: Mimetype.mp4, ptt: false})
         })
         .catch(
           async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
         )
       },
     )
-
-if (Config.WORKTYPE == 'public') {
-    amazone.addCommand({ pattern: 'video ?(.*)', fromMe: false, desc: Lang.MP4}, async (message, match) => {
-
-        const link = match[1]
-    
-        if (!link) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text, {quoted: message.data})
-         await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text)
-        await axios
-          .get(`https://api.zeks.xyz/api/ytplaymp4/2?apikey=VI6j4t4wCbwoc6Deh5wgrJL2Kt1&q=${link}`)
-          .then(async (response) => {
-            const {
-              link,
-            } = response.data.result
-    
-            const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
-    
-            await message.client.sendMessage(message.jid,YTV_UP,MessageType.text, {quoted: message.data});
-            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.document, {mimetype: Mimetype.mp4, ptt: false})
-        })
-        .catch(
-          async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
-        )
-      },
-    )
-}
-*/
+//=====================================================================================
+//=====================================================================================
