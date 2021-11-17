56bcbeaa506a076b0dc27476c999baf32f04d5e3
kavishka <91202334+kavishkaya@users.noreply.github.com>
2021-11-17 18:42:19 +0530

    YT video downloader fix

--- plugins/VIDEO.js
+++ plugins/VIDEO.js
@@ -39,3 +39,55 @@ const tk = Config.WORKTYPE == 'public' ? false : true
 //=====================================================================================
 //=====================================================================================
 */
+
+
+/*
+
+kavishka - neotro
+
+coded by neotro -kavishka  dont copy.amaruwen haduwe ekayi
+
+*/
+
+const neotro = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const { errorMessage, infoMessage } = require('../helpers');
+const YTV_DESC = "Youtube Video Downloader "
+const YT_NEED = "*need word!.*"
+const DWLOAD_VID = "*🎭Downloading Your Video...*"
+const YTV_UP = "*🚀Uploading Your Video...*"
+const NO_RESULT = "*🌀can't Find Anything...*"
+const config = require('../config');
+let KSK = config.WORKTYPE == 'public' ? false : true
+//උස්සන්නද ආවෙ බේසිකයෝ.බොහොම අමාරුවෙන් හැදුවෙ.උස්සන එකා අවජාතකයෙක් කියල හිතාගන්න පුලුවන් පොන්න හැත්ත.හුකන පොන්නයෝ
+
+
+
+
+//info without
+
+
+    neotro.addCommand({ pattern: 'video ?(.*)', fromMe: KSK, deleteCommand: false, dontAddCommandList:true,  deleteCommand: false}, async (message, match) => {
+        const linkk = match[1]
+        if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
+        await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text , {quoted: message.data});
+        await axios
+          .get(`https://rei-api.herokuapp.com/api/dl/ytavv2?url=${linkk}`)
+          .then(async (response) => {
+            const {
+              link,
+            } = response.data.result
+            const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
+            await message.client.sendMessage(message.jid,YTV_UP,MessageType.text , {quoted: message.data});
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {quoted: message.data ,mimetype: Mimetype.mp4, ptt: false})
+        })
+        .catch(
+          async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
+        )
+      },
+    )
+
+
+
+
